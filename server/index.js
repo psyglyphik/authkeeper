d685aa006b8284f342d5480551092f0ffdc1551b
Adam <psyglyphik@gmail.com>
2016-08-10 13:38:45 -0700

    initial commit
     Changes to be committed:
    	new file:   .gitignore
    	new file:   client/.babelrc
    	new file:   client/.gitignore
    	new file:   client/README.md
    	new file:   client/index.html
    	new file:   client/package.json
    	new file:   client/src/actions/index.js
    	new file:   client/src/actions/types.js
    	new file:   client/src/components/admin_area.js
    	new file:   client/src/components/app.js
    	new file:   client/src/components/auth/admin_activation.js
    	new file:   client/src/components/auth/require_admin.js
    	new file:   client/src/components/auth/require_auth.js
    	new file:   client/src/components/auth/signin.js
    	new file:   client/src/components/auth/signout.js
    	new file:   client/src/components/auth/signup.js
    	new file:   client/src/components/default.js
    	new file:   client/src/components/header.js
    	new file:   client/src/components/protected_content.js
    	new file:   client/src/index.js
    	new file:   client/src/reducers/auth_reducer.js
    	new file:   client/src/reducers/index.js
    	new file:   client/style/style.css
    	new file:   client/test/components/app_test.js
    	new file:   client/test/test_helper.js
    	new file:   client/webpack.config.js
    	new file:   server/.gitignore
    	new file:   server/controllers/authentication.js
    	new file:   server/index.js
    	new file:   server/models/user.js
    	new file:   server/package.json
    	new file:   server/router.js
    	new file:   server/services/passport.js

---
+++ server/index.js
@@ -0,0 +1,30 @@
+// Main starting point of the application
+const express = require('express');
+const http = require('http');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const app = express();
+const router = require('./router');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+// DB Setup
+mongoose.connect('mongodb://localhost:auth/auth');
+
+// App Setup
+app.use(morgan('combined'));
+app.use(bodyParser.json({ type: '*/*' }));
+
+// Allow cross-origin resource sharing
+app.use(cors());
+app.options('*', cors());
+
+// Application Routes
+router(app);
+
+
+// Server Setup
+const port = process.env.PORT || 3090;
+const server = http.createServer(app);
+server.listen(port);
+console.log('Server listening on:', port);
